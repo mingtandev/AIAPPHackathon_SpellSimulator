c66effda11d17d6b4deadd884a7bb068859c4cc3
DESKTOP-KCQ2H6B\tanla <tanlalan123@gmail.com>
2020-10-24 10:44:11 +0700

    Movement , apply gravity

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float mouseSensity = 100f;
+    public Transform playerBody;
+
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseLook();
+    }
+
+    void MouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensity * Time.deltaTime;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        xRotation -= mouseY;
+
+        transform.localRotation = Quaternion.Euler(xRotation , 0f , 0f);
+
+        playerBody.Rotate(Vector3.up, mouseX);
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController controller;
+    public float speed = 5f;
+    public Transform groundDetect;
+    public LayerMask GroundLayer;
+    float gravity = -9.8f;
+    Vector3 velocity;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        ApplyVelocity();    
+    }
+
+    void PlayerMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void ApplyVelocity()
+    {
+        //CHECK IS GROUND , velocity  impact by gravity little bit
+        bool isGround = Physics.CheckSphere(groundDetect.position, 0.4f, GroundLayer);
+        if (isGround)
+        {
+            velocity.y = -0.5f;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity);
+    }
+}
